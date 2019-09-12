49f8d841c5fe12c2d17bfd68ce1f33c3121ef8ab
bungernut <bungernut@gmail.com>
2019-09-12 12:19:23 -0700

    latest

--- flask/flaskr/__init__.py
+++ flask/flaskr/__init__.py
@@ -24,9 +24,9 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+# a simple page that says hello
+@app.route('/hello')
+def hello():
+    return 'Hello, World!'
 
-    return app
+return app
